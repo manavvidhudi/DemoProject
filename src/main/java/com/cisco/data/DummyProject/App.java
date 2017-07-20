6909bf0a30280c199fbe2fbc157668922c394573
Mona Lisa <manvesin@partnet.cisco.com>
2017-07-20 12:35:14 +0530

    First commit

---
+++ src/main/java/com/cisco/data/DummyProject/App.java
@@ -0,0 +1,13 @@
+package com.cisco.data.DummyProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
