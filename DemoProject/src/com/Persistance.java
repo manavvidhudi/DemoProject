300cfb79dea950088795d739b7e3e8487f62c4f9
MANAV <manavvidhudi@gmail.com>
2017-11-13 15:30:29 +0530

    inject error

--- DemoProject/src/com/Persistance.java
+++ DemoProject/src/com/Persistance.java
@@ -6,5 +6,6 @@ public class Persistance {
 	public static void main(String[] args) {
 		System.out.println("start persistance");
 		System.out.println("dimag kharabh");
+		System.out.println("d":);
 	}
 }
